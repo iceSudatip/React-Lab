cf7bb5acd7e40bc1816ea49a6ee390f441ed6ff1
sudatiptaweesuk <icesudatip_taweesuk@hotmail.com>
2018-09-17 11:56:00 +0700

    กำลังทำหน้าที่ 4 อยู่ค่ะ

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './CharacterCard';
 
 const word = "Hello";
 class App extends Component {
  render() {
  return (
- <div>
+ <div className= "card">
  {
- Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+     <WordCard value="hello"/>
  }
  </div>
  );
