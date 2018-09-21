2c132d13e28393a07daa6ddb130a66a30c303554
Sudatip <icesudatip_taweesuk@hotmail.com>
2018-09-21 21:44:32 +0700

    แลป7 เพิ่มคำศัพท์

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import IceCoe from './IceCoe';
 
 const word = "swimming";
+const ice="kitty"
 class App extends Component{
   render(){
     return(
       <div>
         <WordCard value={word}/>
+        <IceCoe value={ice}/>
       </div>
     );
   }
