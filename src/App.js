6a253e580e20291e972b0960c313ec4ea4d69b79
Sudatip <icesudatip_taweesuk@hotmail.com>
2018-09-21 17:51:24 +0700

    Game Logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-//import CharacterCard from './CharacterCard';
-import WordCard from "./WordCard";
+import WordCard from './WordCard';
+
 const word = "Hello";
-class App extends Component {
-  
-  render() {
-    return (
+class App extends Component{
+  render(){
+    return(
       <div>
-        {
-          <WordCard value="hello"/>
-        }
-        
+        <WordCard value={word}/>
       </div>
     );
   }
 }
 
 export default App;
-
-
