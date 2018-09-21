2447245fb5fa0d701ccd8fa3193ffda02e7492e9
Sudatip <icesudatip_taweesuk@hotmail.com>
2018-09-21 19:19:54 +0700

    แลป7 ใส่ background

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
 
-const word = "Hello";
+const word = "hello";
 class App extends Component{
   render(){
     return(
